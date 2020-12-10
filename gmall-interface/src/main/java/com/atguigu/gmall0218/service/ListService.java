391f23bdb86c03b9b383dc01f7850ded59853378
LX <1104102296@qq.com>
2020-12-10 10:41:39 +0800

    first..

---
+++ gmall-interface/src/main/java/com/atguigu/gmall0218/service/ListService.java
@@ -0,0 +1,27 @@
+package com.atguigu.gmall0218.service;
+
+import com.atguigu.gmall0218.bean.SkuLsInfo;
+import com.atguigu.gmall0218.bean.SkuLsParams;
+import com.atguigu.gmall0218.bean.SkuLsResult;
+
+public interface ListService {
+
+    /**
+     * 保存数据到es 中！
+     * @param skuLsInfo
+     */
+    void saveSkuLsInfo(SkuLsInfo skuLsInfo);
+
+    /**
+     * 检索数据
+     * @param skuLsParams
+     * @return
+     */
+    SkuLsResult search(SkuLsParams skuLsParams);
+
+    /**
+     * 记录每个商品被访问的次数
+     * @param skuId
+     */
+    void incrHotScore(String skuId);
+}
---
+++ gmall-interface/src/main/java/com/atguigu/gmall0218/service/UserService.java
@@ -0,0 +1,35 @@
+package com.atguigu.gmall0218.service;
+
+import com.atguigu.gmall0218.bean.UserAddress;
+import com.atguigu.gmall0218.bean.UserInfo;
+
+import java.util.List;
+
+public interface UserService {
+    /**
+     * 查询所有数据
+     * @return
+     */
+    List<UserInfo> findAll();
+
+    /**
+     * 根据userId 查询用户地址列表
+     * @param userId
+     * @return
+     */
+    List<UserAddress> getUserAddressList(String userId);
+
+    /**
+     * 登录方法
+     * @param userInfo
+     * @return
+     */
+    UserInfo login(UserInfo userInfo);
+
+    /**
+     * 根据用户Id 查询数据
+     * @param userId
+     * @return
+     */
+    UserInfo verify(String userId);
+}
